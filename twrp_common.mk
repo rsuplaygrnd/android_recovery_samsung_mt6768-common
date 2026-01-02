f16609773c10c381feb3d468eb7c9233dc8b3b3b
rsuntk <rissu.ntk@gmail.com>
2026-01-02 08:46:55 +0700

    mt6768-common: switch to 64bit only build
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- twrp_common.mk
+++ twrp_common.mk
@@ -15,7 +15,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/samsung/mt6768-common/device.mk)
 
